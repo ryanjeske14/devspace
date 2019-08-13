b0d4d726224387651caf1887074329db28be2aed
Ryan Jeske <ryanjeske@gmail.com>
2019-08-13 06:45:10 -0600

    added initial dashboard screen

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import AppContext from "../../AppContext";
 import LandingPage from "../LandingPage/LandingPage";
+import Dashboard from "../Dashboard/Dashboard";
 import NotFoundPage from "../NotFoundPage/NotFoundPage";
+import STORE from "../../STORE";
 
 class App extends Component {
+  state = {
+    user: {},
+    skills: []
+  };
+
+  componentDidMount() {
+    let { user, skills } = STORE;
+    this.setState({
+      user,
+      skills
+    });
+  }
+
   render() {
+    const contextValue = {
+      user: this.state.user
+    };
+
     return (
-      <main className="App" role="main">
-        <Switch>
-          <Route exact path={"/"} component={LandingPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </main>
+      <AppContext.Provider value={contextValue}>
+        <main className="App" role="main">
+          <Switch>
+            <Route exact path={"/"} component={LandingPage} />
+            <Route exact path={"/dashboard"} component={Dashboard} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </main>
+      </AppContext.Provider>
     );
   }
 }
