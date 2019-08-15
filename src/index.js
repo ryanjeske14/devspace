6e553bf2ef6b1711a95070fb35c98a5673da5221
Ryan Jeske <ryanjeske@gmail.com>
2019-08-15 13:51:47 -0600

    capitalized Components folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/App/App";
+import App from "./Components/App/App";
 import "./index.css";
 
 ReactDOM.render(
