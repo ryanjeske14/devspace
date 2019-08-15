87246fc5cb246e229d539fae076f36c14e7ab408
Ryan Jeske <ryanjeske@gmail.com>
2019-08-15 13:38:24 -0600

    modified .gitignore to comment out build

---
+++ build/precache-manifest.de91d04a76967f1af181270c5f0a62ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e048d4f15eb6d13891d9904803dd7263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd1bc6bd3980072a6898",
+    "url": "/static/css/main.5ea08614.chunk.css"
+  },
+  {
+    "revision": "5eb4bcc7fb3cf2ba676b",
+    "url": "/static/js/2.d337eec1.chunk.js"
+  },
+  {
+    "revision": "bd1bc6bd3980072a6898",
+    "url": "/static/js/main.96acd363.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
